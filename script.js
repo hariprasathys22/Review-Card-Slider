ee7a81703d7e39a4853e6c3bfd7adca97946d4b2
hariprasathys22 <confluenceinfotechsolutions@gmail.com>
2022-09-12 14:17:05 +0530

    Completed The project

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const reviews = [
     id: 1,
     name: "Ganesan N",
     job: "Graphic Designer",
-    img: "./images/IMG_1204.JPG",
+    img: "./images/hariprasath.jpg",
     text: "Graphic Designer in Confluence Infotech Solutions,Enterepreneus, Seeking a Challenging position in a reputed organization where I can learn new skills.",
-  },
+    },
   {
     id: 2,
     name: "Laith Mujibar Rahman",
@@ -22,16 +22,16 @@ const reviews = [
   },
   {
     id: 4,
-    name: "Hariharan",
+    name: "Hariharan Y S",
     job: "web developer",
     img: "./images/hariprasath.jpg",
     text: "React Developer in Confluence Infotech Solutions,Enterepreneus, Seeking a Challenging position in a reputed organization where I can learn new skills.",
   },
   {
     id: 5,
-    name: "Sathish",
+    name: "Hariprasath Y S",
     job: "web developer",
-    img: "./images/hariprasath.jpg",
+    img: "./images/IMG_1204.JPG",
     text: "Mern Stack Developer in Confluence Infotech Solutions,Enterepreneus, Seeking a Challenging position in a reputed organization where I can learn new skills.",
   },
 
@@ -48,12 +48,46 @@ const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 
 
-let currentItem =1;
+let currentItem = 0;
 
 
 //load initial item
 window.addEventListener("DOMContentLoaded", function(){
+    showPerson()
+})
+
+function showPerson(){
     const item = reviews[currentItem];
     img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
 
+//show next person
+
+nextBtn.addEventListener('click', function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson();
 })
+prevBtn.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson();
+})
+
+/*
+If we use randow button
+const randomBtn = document.getElementById('random-btn');
+randomBtn.addEventListener('click', function(){
+    const currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson();
+})
+
+
+*/
